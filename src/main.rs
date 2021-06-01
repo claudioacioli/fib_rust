fdcec798cd490dbf632dcc4cb96129668dfe1fb4
ClaudioAcioli <claudio.acioli.silva@gmail.com>
2021-06-01 19:20:35 -0300

    Test fibonacci 100 with rust

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::time::SystemTime;
+
+fn main() {
+    let start = SystemTime::now();
+    fib(100);
+    let end = SystemTime::now();
+    let diff = end.duration_since(start).expect("...");
+    println!("time: {:?}", diff);
+}
+
+fn fib(n: u32) -> u32 {
+    if n < 2 {
+        n
+    } else {
+        fib(n - 1) + fib(n -2)
+    }
+}
