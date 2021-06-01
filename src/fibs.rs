0c53da67115e0bb4310681c25c3b26e58466e7bb
ClaudioAcioli <claudio.acioli.silva@gmail.com>
2021-06-01 20:03:10 -0300

    feat: Create file for functions

---
+++ src/fibs.rs
@@ -0,0 +1,21 @@
+pub fn fib(n: u64) -> u64 {
+    if n < 2 {
+        n
+    } else {
+        fib(n - 1) + fib(n - 2)
+    }
+}
+
+pub fn fib_loop(n: u64) -> u64 {
+    let mut i = 0;
+    let mut x = 1;
+    let mut y = 0;
+    let mut aux;
+    while i < n {
+        aux = x + y;
+        y = x;
+        x = aux;
+        i = i + 1;
+    }
+    x
+}
