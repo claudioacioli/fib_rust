019cdb4b139d805c85e069e0a714d935757c1646
ClaudioAcioli <claudio.acioli.silva@gmail.com>
2021-06-01 19:30:04 -0300

    fix: Change to 64

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use std::time::SystemTime;
 
 fn main() {
     let start = SystemTime::now();
-    fib(100);
+    fib(30);
     let end = SystemTime::now();
     let diff = end.duration_since(start).expect("...");
     println!("time: {:?}", diff);
 }
 
-fn fib(n: u128) -> u128 {
+fn fib(n: u64) -> u64 {
     if n < 2 {
         n
     } else {
