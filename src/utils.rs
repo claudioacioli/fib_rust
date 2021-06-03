3fd07259e0343a77aac9ce06dfd28723c59f0b1f
ClaudioAcioli <claudio.acioli.silva@gmail.com>
2021-06-03 12:19:05 -0300

    feat: Add function to wrap the comparation

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::str::FromStr;
+use std::time::SystemTime;
 
 pub fn get_n() -> u128 {
     let mut number:String = String::new();
@@ -7,4 +8,15 @@ pub fn get_n() -> u128 {
     io::stdin().read_line(&mut number).expect("Nothing to read...");
     u128::from_str(&number.trim()).unwrap_or(0)
 //    return number.trim().parse::<u128>().unwrap_or(0);
+}
+
+pub fn compare (f: fn(u128) -> u128, n:u128) {
+    let start = SystemTime::now();
+    f(n);
+    let end = SystemTime::now();
+    println!(
+        "time {:?} to calculate the fibonacci of {}",
+        end.duration_since(start).expect("..."),
+        n
+    );
 }
