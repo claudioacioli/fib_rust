83042d6f0cb5bbe42ed0373536e9f8ab2c98f364
ClaudioAcioli <claudio.acioli.silva@gmail.com>
2021-06-03 12:17:46 -0300

    refactor: Change convert method

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,10 @@
 use std::io;
+use std::str::FromStr;
 
-pub fn get_n() -> u64 {
-    let mut string_number = String::new();
+pub fn get_n() -> u128 {
+    let mut number:String = String::new();
     println!("Please input a number:");
-    io::stdin().read_line(&mut string_number).expect("Nothing to read..");
-    let n = string_number.trim().parse::<u64>().unwrap();
-    n
+    io::stdin().read_line(&mut number).expect("Nothing to read...");
+    u128::from_str(&number.trim()).unwrap_or(0)
+//    return number.trim().parse::<u128>().unwrap_or(0);
 }
